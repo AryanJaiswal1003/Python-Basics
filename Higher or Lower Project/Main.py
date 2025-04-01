1b97aff988ced7fe88e4ce7c56c465db7de83540
Aryan Jaiswal <arihantjaiswal1003@gmail.com>
2025-04-01 19:24:55 +0530

    Changes Made

--- Higher or Lower Project/Main.py
+++ Higher or Lower Project/Main.py
@@ -1,41 +1,54 @@
 #link = https://appbrewery.github.io/python-day14-demo/
 
 #TODO-1: Generate Random data and Compare it with other data
+from game_data import data
 from art import logo , vs
-from gamedata import data
+import random
 
-# change = []
-# for char in data:
-#     change.append(char['name'])
-# print(change)
 
-def compare():
-    game_over = False
-    number = 0
-    while not game_over:
-        Name = data[number]['name']
-        Description = data[number]['description']
-        Country = data[number]['country']
-        print(f"Compare A: {Name}, a {Description}, from {Country}")
-        number += 2
-        if number == 50:
-            game_over = True
-        else:
-            continue
+def assertion(num):
+    user = data[num]['name']
+    user_description = data[num]['description']
+    user_country = data[num]['country']
+    return f"Compare A: {user}, a {user_description}, from {user_country}"
 
-def against():
-    game_over = False
-    number = 1
-    while not game_over:
-        Name = data[number]['name']
-        Description = data[number]['description']
-        Country = data[number]['country']
-        print(f"Against B: {Name}, a {Description}, from {Country}")
-        number += 2
-        if number == 51:
-            game_over = True
-        else:
-            continue
 
-compare()
-against()
+def against(num):
+    print(vs)
+    user = data[num]['name']
+    user_description = data[num]['description']
+    user_country = data[num]['country']
+    return f"Against B: {user}, a {user_description}, from {user_country}"
+
+
+index1 = random.randrange(0 , 50)
+game_over = False
+user_score = 0
+
+while not game_over:
+    index2 = random.randrange(0, 50)
+    a = data[index1]['follower_count']
+    b = data[index2]['follower_count']
+
+    print(logo)
+    if user_score > 0:
+        print(f"You're Right! Current Score: {user_score}")
+
+    print(assertion(num=index1))
+    print(against(num=index2))
+
+    user_input = input("Who has More Followers? Type 'A' or 'B' = ").lower()
+    print("\n" * 20)
+
+    if user_input == "a" and a > b:
+        index1 = index2
+        user_score += 1
+        continue
+    if user_input == "b" and b > a:
+        index1 = index2
+        user_score += 1
+        continue
+    else:
+        print(logo)
+        print(f"Sorry, that's wrong. Final score: {user_score}")
+        break
