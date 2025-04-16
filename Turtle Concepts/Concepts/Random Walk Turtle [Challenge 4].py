9252e462e8892d6906c76273113be8ae573f8c45
AryanJaiswal1003 <arihantjaiswal1003@gmail.com>
2025-04-16 17:52:19 +0530

    Changes made

--- Turtle Concepts/Concepts/Random Walk Turtle [Challenge 4].py
+++ Turtle Concepts/Concepts/Random Walk Turtle [Challenge 4].py
@@ -4,19 +4,21 @@ from turtle import Turtle , Screen
 
 t = Turtle()
 t.shape("turtle")
+screen = Screen()
+screen.bgcolor('black')
 
 angle = [0 , 90 , 180 , 270]
 colors = ["SteelBlue" , "PaleGreen" , "OliveDrab" , "DarkOrange" , "Gold" , "Maroon" , "DarkMagenta" ,
            "MediumVioletRed" , "Indigo" , "DarkSlateBlue" , "LightCoral" , "OrangeRed" , "Firebrick" ,
-          "DarkGoldenrod", "OldLace" , "DarkSlateGray" , "LightSkyBlue" , "MediumSpringGreen" , "Black" , "SlateGray"]
+          "DarkGoldenrod", "OldLace" , "DarkSlateGray" , "LightSkyBlue" , "MediumSpringGreen", "SlateGray"]
 
 t.pensize(15)
 t.speed("fastest")
 
-for char in range(2000):
+for char in range(500):
     t.forward(20)
     t.color(random.choice(colors))
     t.setheading(random.choice(angle))
 
-screen = Screen()
+
 screen.exitonclick()
